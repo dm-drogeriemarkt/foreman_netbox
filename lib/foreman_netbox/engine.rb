96122c0bb5128152190533a15500b7812900a426
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2019-11-12 16:44:17 +0100

    Travis setup

--- lib/foreman_netbox/engine.rb
+++ lib/foreman_netbox/engine.rb
@@ -16,11 +16,5 @@ module ForemanNetbox
         requires_foreman '>= 1.24'
       end
     end
-
-    initializer 'foreman_netbox.register_gettext', after: :load_config_initializers do |_app|
-      locale_dir = File.join(File.expand_path('../..', __dir__), 'locale')
-      locale_domain = 'foreman_netbox'
-      Foreman::Gettext::Support.add_text_domain locale_domain, locale_dir
-    end
   end
 end
