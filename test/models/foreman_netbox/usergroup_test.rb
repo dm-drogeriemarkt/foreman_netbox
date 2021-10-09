0752b41bdd26d4195ea6356405a001311185a9da
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2021-10-09 13:18:37 +0200

    Increase max length of tenant name

--- test/models/foreman_netbox/usergroup_test.rb
+++ test/models/foreman_netbox/usergroup_test.rb
@@ -7,13 +7,13 @@ module ForemanNetbox
     describe '#netbox_tenant_name' do
       let(:usergroup) { FactoryBot.build_stubbed(:usergroup, id: 1, name: usergroup_name) }
 
-      context 'when name is longer than 30' do
-        let(:usergroup_name) { SecureRandom.hex(16) }
+      context 'when name is longer than 100' do
+        let(:usergroup_name) { SecureRandom.hex(64) }
 
-        it { assert_equal "#{usergroup.name.truncate(28, omission: '')}-#{usergroup.id}", usergroup.netbox_tenant_name }
+        it { assert_equal "#{usergroup.name.truncate(98, omission: '')}-#{usergroup.id}", usergroup.netbox_tenant_name }
       end
 
-      context 'when name is shorter than 30' do
+      context 'when name is shorter than 100' do
         let(:usergroup_name) { 'Name' }
 
         it { assert_equal usergroup.name, usergroup.netbox_tenant_name }
